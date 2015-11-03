536c2d46d28dbd47e445195dd806111f894d16e3
Ken Stowell <stowell.kt@gmail.com>
2015-11-02 23:50:51 -0800

    Simplifying the regex a tad.

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ module.exports = function(str) {
     str = (str && str.constructor === String)? str : '';
    	
     // Subsequent validation not needed as loop will never enter if '.' doesn't exist
-    (str.match(/\{\{[a-f0-9]*.*?\}\}/g) || []).forEach(function(match) {
+    (str.match(/\{+[a-f0-9]*.*?\}+/g) || []).forEach(function(match) {
       var search = (match.match(/[a-f0-9]*\.+[^\}\s]*/g) || [])[0],
           segments = search && search.split('.'),
           text = (map && map[segments[0]] && map[segments[0]][segments[1]]) || map.default;
